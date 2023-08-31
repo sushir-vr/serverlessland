dc8a2289f0600f809f4d1cd2f6951f48612d8615
EC2 Default User <ec2-user@ip-172-31-2-86.us-east-2.compute.internal>
2023-08-31 06:30:10 +0000

    first test

--- svlsland/svlsland_stack.py
+++ svlsland/svlsland_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_lambda as lambda_,
+    aws_kinesis as kinesis,
+    aws_lambda_event_sources as event_sources,
+    Stack, 
+    Duration
 )
 from constructs import Construct
 
@@ -11,9 +13,25 @@ class SvlslandStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        with open("lambda_function.py", encoding="utf8") as fp:
+            handler_code = fp.read()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SvlslandQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        kinesis_stream = kinesis.Stream(self, "SampleStream")
+        
+        lambdaFn = lambda_.Function(
+            self, 'sampleFn',
+            handler='index.lambda_handler',
+            code=lambda_.InlineCode(handler_code),
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            timeout=Duration.seconds(30)
+        )
+
+        kinesis_stream.grant_read(lambdaFn)
+        
+        kinesis_event_source = event_sources.KinesisEventSource(
+            stream=kinesis_stream,
+            starting_position=lambda_.StartingPosition.LATEST,
+            batch_size=1
+        )
+        
+        lambdaFn.add_event_source(kinesis_event_source)
